07d41912b617a96b2a7fdc59333354deee11953f
amdev <56517440+thereisnodeveloper@users.noreply.github.com>
2024-11-11 19:15:24 -0500

    set up Node class

--- src/index.js
+++ src/index.js
@@ -2,3 +2,10 @@
 import './reset.css';
 import './style.css';
 
+export default class NodeBst {
+  constructor(data, left = null, right = null) {
+    this.data = data;
+    this.left = left;
+    this.right = right;
+  }
+}
--- src/index.test.js
+++ src/index.test.js
@@ -1,11 +1,15 @@
 import NodeBst from './index.js';
 
 describe('Node class', () => {
+  const node1 = new NodeBst();
+
   it('exists', () => {
     expect(NodeBst).toBeDefined();
   });
-  it('is a class'), () => {
-    const node1 = new NodeBst();
-    expect(node1).toBeInstanceOf(NodeBst)
-  };
+  it('is a class', () => {
+    expect(node1).toBeInstanceOf(NodeBst);
+  });
+  it('has properties', () => {
+    ['data', 'left', 'right'].forEach((property) => expect(node1).toHaveProperty(property));
+  });
 });
